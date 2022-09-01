0b1696d04c38163c33d4a9e53237637a3c743977
CG-SS <criguiss@uol.com.br>
2022-09-01 19:18:25 -0300

    Creating initial project layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+mod routes;
+use crate::routes::get::greet;
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(greet)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
